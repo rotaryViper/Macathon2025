0c311f675ad58e5e166abfeb8d0d8c2b94adc4e5
jlam <lamkaizhe@gmail.com>
2025-04-25 17:54:52 +1000

    Login page
    Finished login page + css styling

--- server.js
+++ server.js
@@ -7,11 +7,17 @@ const VIEWS_PATH = path.join(__dirname, "/views/");
 
 app.use(express.static("node_modules/bootstrap/dist/css"));
 
+app.use(express.static('public'));
 
 // Routes 
 // endpoint to the home page
 app.get('/', (req, res) => {
     res.sendFile(VIEWS_PATH + "index.html");
+})
+
+// route to login page
+app.get('/login', (req, res) => {
+    res.sendFile(VIEWS_PATH + "login.html");
 })
 
 app.listen(PORT_NUMBER, () => {
